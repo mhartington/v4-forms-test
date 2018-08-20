b4c2446425d1d60a191a90dfd17894ef915f8e70
mhartington <mikehartington@gmail.com>
2018-08-20 10:17:41 -0400

    add forms

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  todo = {};
+  fbTodo: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.fbTodo = this.formBuilder.group({
+      title: ['', Validators.required],
+      description: ['']
+    });
+  }
 
+  logFBForm(){
+     console.log(this.fbTodo.value);
+  }
+  logForm() {
+    console.log(this.todo);
+  }
 }
